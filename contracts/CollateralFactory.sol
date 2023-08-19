17e18dea450d279e171e10b33be0fee57c7897cb
maxvia87 <maximeviard@hotmail.fr>
2023-08-19 12:25:10 +0200

    feat: set up Collateral & Collateral Factory

---
+++ contracts/CollateralFactory.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {CollateralWrapper} from "./CollateralWrapper.sol";
+import {Clones} from "@openzeppelin/contracts/proxy/Clones.sol";
+
+/**
+ * @title Collateral Factory
+ */
+contract CollateralFactory {
+    /**************************************************************************/
+    /* State */
+    /**************************************************************************/
+
+    address public immutable poolFactory;
+
+    address public immutable collateralImplementationContract = address(new CollateralWrapper());
+
+    /* Collateral wrapper address for a specific collection */
+    mapping(address => address) public collateralWrapper;
+
+    /**************************************************************************/
+    /* Events */
+    /**************************************************************************/
+
+    /**
+     * @notice Emitted when a new collateral wrapper is created
+     * @param collection Address of the collection
+     * @param collateralWrapper Address of the new collateral wrapper
+     */
+    event CollateralWrapperCreated(address indexed collection, address indexed collateralWrapper);
+
+    /**************************************************************************/
+    /* Initialization */
+    /**************************************************************************/
+
+    constructor() {
+        poolFactory = msg.sender;
+    }
+
+    /**************************************************************************/
+    /* Modifiers */
+    /**************************************************************************/
+
+    modifier onlyPoolFactory() {
+        require(msg.sender == poolFactory, "CollateralWrapper: Only pool factory can call");
+        _;
+    }
+
+    /**************************************************************************/
+    /* Implementation */
+    /**************************************************************************/
+
+    function deployCollateralWrapper(
+        address collection_
+    ) external onlyPoolFactory returns (address collateralWrapperAddress) {
+        /* Check if collateral wrapper already exists */
+        require(
+            collateralWrapper[collection_] == address(0),
+            "CollateralWrapper: Collateral wrapper already exists"
+        );
+
+        /* Deploy collateral wrapper */
+        collateralWrapperAddress = Clones.clone(collateralImplementationContract);
+
+        /* Initialize collateral wrapper */
+        CollateralWrapper(collateralWrapperAddress).initialize(collection_, poolFactory);
+
+        /* Update collateralWrapper mapping to avoid deploying twice the  */
+        collateralWrapper[collection_] = collateralWrapperAddress;
+
+        /* Emit event */
+        emit CollateralWrapperCreated(collection_, collateralWrapperAddress);
+    }
+}
---
+++ contracts/CollateralWrapper.sol
@@ -0,0 +1,144 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+/**
+ * @title Collateral Wrapper
+ */
+contract CollateralWrapper is ERC721, ReentrancyGuard, Initializable {
+    /**************************************************************************/
+    /* State */
+    /**************************************************************************/
+
+    address public poolFactory;
+
+    address[] public pools;
+
+    address public collection;
+
+    /**************************************************************************/
+    /* Events */
+    /**************************************************************************/
+
+    /**
+     * @notice Emitted when a pool is added
+     * @param pool Address of the pool
+     */
+    event AddPool(address pool);
+
+    /**
+     * @notice Emitted when the NFT is unwrapped
+     * @param tokenId Token ID of the NFT collateral wrapper token
+     * @param account Address that unwrapped the NFT
+     */
+    event NFTBurnt(uint256 indexed tokenId, address indexed account);
+
+    /**
+     * @notice Emitted when NFT is minted
+     * @param tokenId Token ID of the new collateral wrapper token
+     * @param account Address that created the NFT
+     */
+    event NFTMinted(uint256 indexed tokenId, address indexed account);
+
+    /**************************************************************************/
+    /* Initialization */
+    /**************************************************************************/
+
+    constructor() ERC721("Nemeos Wrapper", "NMOSW") {
+        _disableInitializers();
+    }
+
+    function initialize(address collection_, address poolFactory_) external onlyInitializing {
+        collection = collection_;
+        poolFactory = poolFactory_;
+    }
+
+    /**************************************************************************/
+    /* Modifiers */
+    /**************************************************************************/
+
+    modifier onlyPool() {
+        require(existsInPools(msg.sender), "CollateralWrapper: Only pool can call");
+        _;
+    }
+
+    modifier onlyPoolFactory() {
+        require(msg.sender == poolFactory, "CollateralWrapper: Only pool factory can call");
+        _;
+    }
+
+    /**************************************************************************/
+    /* Implementation */
+    /**************************************************************************/
+
+    /**
+     * @notice Check if token ID exists
+     * @param tokenId Token ID
+     * @return True if token ID exists, otherwise false
+     */
+    function exists(uint256 tokenId) external view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    function existsInPools(address pool_) public view returns (bool) {
+        for (uint256 i = 0; i < pools.length; i++) {
+            if (pools[i] == pool_) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**************************************************************************/
+    /* Pool API */
+    /**************************************************************************/
+
+    /**
+     * @notice Burn NFT
+     *
+     * Emits a {NFTBurnt} event
+     *
+     * @dev Only pool can call
+     * @param tokenId NFT token ID
+     */
+    function burn(uint256 tokenId) external nonReentrant onlyPool {
+        _burn(tokenId);
+
+        emit NFTBurnt(tokenId, msg.sender);
+    }
+
+    /**
+     * @notice Deposit NFT collateral into contract and mint token
+     *
+     * Emits a {NFTMinted} event
+     *
+     * @dev Collateral token and token ids
+     * @param tokenId_ NFT token IDs
+     * @param receiver_ Address that receives the NFT
+     */
+    function mint(
+        uint256 tokenId_,
+        address receiver_
+    ) external nonReentrant onlyPool returns (uint256) {
+        /* Mint token */
+        _mint(receiver_, tokenId_);
+
+        emit NFTMinted(tokenId_, receiver_);
+
+        return tokenId_;
+    }
+
+    /**************************************************************************/
+    /* Pool Factory API */
+    /**************************************************************************/
+
+    function addPool(address pool) external onlyPoolFactory {
+        pools.push(pool);
+
+        emit AddPool(pool);
+    }
+}
