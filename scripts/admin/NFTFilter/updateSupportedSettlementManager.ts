49de778e2a02674cb3b9a20d8f90500dd46a0e0c
rigwild <me@rigwild.dev>
2024-01-06 18:07:24 +0100

    Update NFT_FILTER_ADDRESS in script

--- scripts/admin/NFTFilter/updateSupportedSettlementManager.ts
+++ scripts/admin/NFTFilter/updateSupportedSettlementManager.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 
-const NFT_FILTER_ADDRESS = '0x90d5e0cd6E36E274Ed801F1b5a01Af0f2e379D10';
+const NFT_FILTER_ADDRESS = '0xCaaCaf88244cDb7835568DE966CeBA6657fe02AC';
 const ALLOWED_SETTLEMENT_MANAGERS = [
   '0xC385568955b1434207bbC598026Da0d145aC7436',
   '0x9B213ca33427697D7D5FD3e3d7b6377b673168DD',
@@ -8,8 +8,25 @@ const ALLOWED_SETTLEMENT_MANAGERS = [
 ];
 
 async function main() {
-  const poolFactory = await ethers.getContractAt('NFTFilter', NFT_FILTER_ADDRESS);
-  const tx = await poolFactory.updatesupportedSettlementManagers(ALLOWED_SETTLEMENT_MANAGERS);
+  const nftFilter = await ethers.getContractAt('NFTFilter', NFT_FILTER_ADDRESS);
+
+  console.log(
+    'supportedSettlementManagers[0]',
+    await nftFilter.supportedSettlementManagers(0).catch(() => {}),
+  );
+  console.log(
+    'supportedSettlementManagers[1]',
+    await nftFilter.supportedSettlementManagers(1).catch(() => {}),
+  );
+  console.log(
+    'supportedSettlementManagers[2]',
+    await nftFilter.supportedSettlementManagers(2).catch(() => {}),
+  );
+  // console.log('supportedSettlementManagers[3]', await nftFilter.supportedSettlementManagers(3));
+
+  console.log('protocolAdmin', await nftFilter.protocolAdmin());
+
+  const tx = await nftFilter.updatesupportedSettlementManagers(ALLOWED_SETTLEMENT_MANAGERS);
   await tx.wait(1);
   console.log('tx:', tx);
 }
